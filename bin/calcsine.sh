722ee50dfae727d29ac1104a7c7c2a9c0a4f257e
nm-jp <118420278+nm-jp@users.noreply.github.com>
2023-05-05 11:30:54 +0900

    change the treatment of the length parameter

--- bin/calcsine.sh
+++ bin/calcsine.sh
@@ -7,15 +7,14 @@ set -eu
 
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
-	Usage   : ${0##*/} [パラメータ]
-	Options : -t<周期> -o<位相> -a<振幅> -d<出力長>
+	Usage   : ${0##*/} [出力長]
+	Options : -t<周期> -i<位相> -a<振幅>
 
 	正弦の値を計算する。
 
 	-tオプションで周期の長さ（ピクセル）を指定できる。デフォルトは40ピクセル。
-	-oオプションで初期位相（度数法）を指定できる。デフォルトは0。
+	-iオプションで初期位相（度数法）を指定できる。デフォルトは0。
 	-aオプションで振幅（ピクセル）を指定できる。デフォルトは10ピクセル。
-	-dオプションで出力長（ピクセル）を指定できる。デフォルトは40ピクセル。
 	USAGE
   exit 1
 }
@@ -27,9 +26,8 @@ print_usage_and_exit () {
 # 変数を初期化
 opr=''
 opt_t='40'
-opt_o='0'
+opt_i='0'
 opt_a='10'
-opt_d='40'
 
 # 引数をパース
 i=1
@@ -38,9 +36,8 @@ do
   case "$arg" in
     -h|--help|--version) print_usage_and_exit ;;
     -t*)                 opt_t=${arg#-t}      ;;
-    -o*)                 opt_o=${arg#-o}      ;;
+    -i*)                 opt_i=${arg#-i}      ;;
     -a*)                 opt_a=${arg#-a}      ;;
-    -d*)                 opt_d=${arg#-d}      ;;
     *)
       if [ $i -eq $# ] && [ -z "$opr" ]; then
         opr=$arg
@@ -54,12 +51,11 @@ do
   i=$((i + 1))
 done
 
-# 有効なパラメータ指定か確認
-# if ! printf '%s\n' "$opr" | grep -Eq '^[0-9]+,[0-9]+,[0-9]+,[0-9]+$'; then
-#   echo "${0##*/}: \"$opr\" invalid parameter" 1>&2
-#   exit 21
-# fi
-
+# 有効な数値であるか判定
+if ! printf '%s\n' "$opr" | grep -Eq '^[0-9]+$'; then
+  echo "${0##*/}: \"$opr\" invalid number" 1>&2
+  exit 21
+fi
 
 # 有効な数値であるか判定
 if ! printf '%s\n' "$opt_t" | grep -Eq '^[0-9]+$'; then
@@ -68,8 +64,8 @@ if ! printf '%s\n' "$opt_t" | grep -Eq '^[0-9]+$'; then
 fi
 
 # 有効な数値であるか判定
-if ! printf '%s\n' "$opt_o" | grep -Eq '^[0-9]+$'; then
-  echo "${0##*/}: \"$opt_o\" invalid number" 1>&2
+if ! printf '%s\n' "$opt_i" | grep -Eq '^[0-9]+$'; then
+  echo "${0##*/}: \"$opt_i\" invalid number" 1>&2
   exit 41
 fi
 
@@ -79,18 +75,11 @@ if ! printf '%s\n' "$opt_a" | grep -Eq '^[0-9]+$'; then
   exit 51
 fi
 
-# 有効な数値であるか判定
-if ! printf '%s\n' "$opt_d" | grep -Eq '^[0-9]+$'; then
-  echo "${0##*/}: \"$opt_d\" invalid number" 1>&2
-  exit 61
-fi
-
 # パラメータを決定
-param=$opr
+len=$opr
 period=$opt_t
-phase=$opt_o
+iniphase=$opt_i
 amp=$opt_a
-duration=$opt_d
 
 ######################################################################
 # 本体処理
@@ -100,11 +89,12 @@ gawk '
 BEGIN {
   # パラメータを設定
   period   = '"${period}"';
-  phase    = '"${phase}"';
+  iniphase = '"${iniphase}"';
   amp      = '"${amp}"';
-  duration = '"${duration}"';
+  len      = '"${len}"';
 
-  pi  = 3.1415;
+  # 定数を定義
+  pi  = 3.141592;
   pi2 = pi * 2.0;
 
   # ピクセルあたりの物理次元の長さ（浮動小数点数）
@@ -113,24 +103,22 @@ BEGIN {
   # 物理次元での振幅（浮動小数点数）
   physamp = 1.0 * amp;
 
-  # 物理次元での初期位相（浮動小数点数）
-  physphase = pi2 * phase / 360.0;
+  # 弧度法での初期位相（浮動小数点数）
+  radiniphase = pi2 * iniphase / 360.0;
 
-  for (i = 1; i <= duration; i++) {
+  for (i = 1; i <= len; i++) {
     # 浮動小数点数での計算
-    buf[i] = physamp * sin((i-1) * physperiod + physphase);
+    buf[i] = physamp * sin((i-1) * physperiod + radiniphase);
   }
 
-  # 出力
-  for (i = 1; i <= duration; i++) {
-    print round(buf[i]), i;
-  }
+  # 値を丸めて出力
+  for (i = 1; i <= len; i++) { print round(buf[i]); }
 
   exit;
 }
 
 function round(x) {
-  if (x >= 0) { return int(x);       }
-  else        { return -1*int(-1*x); }
+  if (x >= 0) { return int(x + 0.5);       }
+  else        { return -1*int(-1*x + 0.5); }
 }
 '
